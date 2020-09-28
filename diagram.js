02a9979af7e825f33cbe4e127f293b502d9c7704
windowsloth <67842472+windowsloth@users.noreply.github.com>
2020-09-27 21:27:43 -0400

    Update diagram.js

--- diagram.js
+++ diagram.js
@@ -4,10 +4,10 @@ let h = 600;
 function setup() {
   w = windowWidth;
   const canvas = createCanvas(w, h);
-  const boundary = new Block(-w / 2, -h / 2, w, h);
   canvas.parent('container');
 }
 
 function draw() {
+  const boundary = new Block(-w / 2, -h / 2, w, h);
   let tree = new Quadtree(boundary, 4);
 }
