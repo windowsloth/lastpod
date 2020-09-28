5a21995aa8105d48a30baffe75bd6f9fc8885f7e
windowsloth <67842472+windowsloth@users.noreply.github.com>
2020-09-27 21:30:30 -0400

    Update diagram.js

--- diagram.js
+++ diagram.js
@@ -1,6 +1,15 @@
 let w;
 let h = 600;
 
+let total = data.length;
+let most = 0;
+for (let person of data) {
+  if (person.cons.length > most) {
+    most = person.cons.length
+  }
+  person.todraw = person.cons.slice(0, person.cons.length);
+}
+
 function setup() {
   w = windowWidth;
   const canvas = createCanvas(w, h);
@@ -10,4 +19,15 @@ function setup() {
 function draw() {
   const boundary = new Block(-w / 2, -h / 2, w, h);
   let tree = new Quadtree(boundary, 4);
+}
+
+function drawDots(arr) {
+  for (let i = 0; i < cols * rows; i++) {
+    grid.push(null);
+  }
+  for (let person of arr) {
+    person.sel = false;
+    person.score = person.cons.length / most;
+    // stickPin(person, person.score);
+  }
 }
